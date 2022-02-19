82fc9021a278c7541f35d64d4f67e45192d7d468
Mr. Sciame <35709490+sciame@users.noreply.github.com>
2022-02-19 13:59:01 +0000

    added a "win screen" if the difference is 0 and you guessed correctly

--- pre02/Guess.java
+++ pre02/Guess.java
@@ -7,23 +7,27 @@ class Guess {
   int D;
 
   Random random = new Random();
-  int number = random.nextInt(100) +1;
+  int number = random.nextInt(1) +1;
   System.out.println(number);
    
   Scanner in = new Scanner(System.in);
 
   System.out.print("Guess a number between 1 and 100, inclusive.\nType a number: ");
   userin = in.nextInt();
+  //include the following line after entering an int variable into the Scanner
   in.nextLine(); 
 
 
 
   System.out.println("Your guess is: " + userin + "\nThe computer generated: " + number);     
-  D = number - userin;
+ 
 
-  D = Math.abs(D);
+  D = Math.abs(number - userin);
   
   System.out.println("\nThe difference between you and the computer is: " + D);
+  if(number==userin){
+    System.out.println("\nNIIIIIIIIIIIICCCCCCCCE!!!!!!");
+  }
 
 }
 
