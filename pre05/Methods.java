b01d0ddd2e1bcb91cd1b34bc632645be73427c6a
Mr. Sciame <35709490+sciame@users.noreply.github.com>
2022-04-27 00:04:20 +0000

    I think I got the Ackerman method to work, and I also moved on to the pre06 folder problems (Think Java 8: exercise 1)

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -19,9 +19,23 @@ public class Methods {
       }
     }
 
+
+    public static int ack(int m, int n){
+        if(m==0){
+        return n +1;
+          } else if(m>0 && n==0){
+          return ack(m-1,1);
+          } else if(m>0 && n>0){
+          return ack(m-1,ack(m,n-1));
+          } else {
+          return -999;
+          }
+        }
+  
     public static void main(String[] args){
       System.out.println(isDivisible(50,5));
       System.out.println(isTriangle(3,4,5));
+      System.out.println(ack(3,3));
         }
 	
 }
