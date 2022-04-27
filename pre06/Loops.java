829dc130b5ba12edc390d045f6a55c6c15dff91e
Mr. Sciame <35709490+sciame@users.noreply.github.com>
2022-04-27 20:21:11 +0000

    all the activities from pre06 seem to be working properly...although I didn't account for the possibility of someone attempting to use negative integers

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -12,9 +12,32 @@ public static double squareRoot(double a, double x, double x1) {
   return x1;
 }
 
+public static double power(double x, int n) {
+ double answer = x;
+  if(n == 0){
+   return 1;
+ } else {
+   for(int i=1; i<n; i++){
+     answer = answer * x;
+   }
+   } 
+   return answer;
+ }
+ 
+
+  
+public static int factorial(int n) {
+  int answer = n; 
+  for(int i = n-1; i>0; i--){
+   answer *= i;
+   }
+ return answer;
+}
+  
 public static void main(String[] args) {
     System.out.println(squareRoot(81, 40, 40));
-  
+    System.out.println(power(3, 3));
+    System.out.println(factorial(5));
 }
   
 }
